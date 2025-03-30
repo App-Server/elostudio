45e653f97f6120c9b51bfe1090494ea658d9c56a
Aragao <epicappcode@gmail.com>
2025-03-30 11:39:16 -0300

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <Navbar />
+        <div class="container">
+            <h1>Home</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+import Navbar from '../components/Navbar.vue';
+
+export default {
+    components: {
+        Navbar,
+    }
+}
+</script>
